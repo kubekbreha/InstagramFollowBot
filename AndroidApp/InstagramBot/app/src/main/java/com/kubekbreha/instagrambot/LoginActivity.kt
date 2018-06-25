3e76015e698910908e8c5b503ae83c3673a66238
Jakub Brehuv <jakub.brehuv@ibm.com>
2018-06-25 17:03:37 +0200

    login remaked and user logged button checker

--- AndroidApp/InstagramBot/app/src/main/java/com/kubekbreha/instagrambot/LoginActivity.kt
+++ AndroidApp/InstagramBot/app/src/main/java/com/kubekbreha/instagrambot/LoginActivity.kt
@@ -1,21 +1,18 @@
 package com.kubekbreha.instagrambot
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import dev.niekirk.com.instagram4android.Instagram4Android
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.progressDialog
 import org.jetbrains.anko.toast
 
 
-class LoginActivity : AppCompatActivity() , View.OnClickListener{
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var username: String
     private lateinit var password: String
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,42 +23,22 @@ class LoginActivity : AppCompatActivity() , View.OnClickListener{
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id) {
+        when (v!!.id) {
             R.id.activity_login_button_logIn -> {
                 val dialog = progressDialog(message = "Please wait a bit ...", title = "Logging in ...")
 
                 dialog.show()
                 username = activity_login_editText_username.text.toString()
-                password = activity_login_editText_username.text.toString()
+                password = activity_login_editText_password.text.toString()
+                toast(username + " " + password)
 
-                val instagramUser = Instagram4Android.builder().username(username).password(password).build()
-
-                val thread = Thread(Runnable {
-                    try {
-
-                        instagramUser.setup()
-                        instagramUser.login()
-
-
-                        if (instagramUser.isLoggedIn){
-//                            val intentInstagramUser = InstagramUser(instagramUser)
-
-                            val intent = Intent(this, MainActivity::class.java)
-//                            intent.putExtra("instagramUser", intentInstagramUser)
-                            startActivity(intent)
-                        }
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }finally {
-                        dialog.dismiss()
-                    }
-                })
-
-
-                thread.start()
-
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("userName", username)
+                intent.putExtra("userPassword", password)
+                startActivity(intent)
 
             }
         }
     }
+
 }
--- AndroidApp/InstagramBot/app/src/main/java/com/kubekbreha/instagrambot/MainActivity.kt
+++ AndroidApp/InstagramBot/app/src/main/java/com/kubekbreha/instagrambot/MainActivity.kt
@@ -2,19 +2,47 @@ package com.kubekbreha.instagrambot
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import dev.niekirk.com.instagram4android.Instagram4Android
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.progressDialog
 import org.jetbrains.anko.toast
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var username: String
+    private lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val bundle = intent.extras
-//        val instagramUser = bundle.get("instagramUser")
-//
-//        toast(instagramUser.toString())
+        //get values from loginActivity
+        val bundle = intent.extras
+        username = bundle.get("userName") as String
+        password = bundle.get("userPassword") as String
+
+        //start logging
+        val dialog = progressDialog(message = "Please wait a bit ...", title = "Logging in ...")
+        dialog.show()
+
+        val instagramUser = Instagram4Android.builder().username(username).password(password).build()
+        val thread = Thread(Runnable {
+            try {
+                instagramUser.setup()
+                instagramUser.login()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }finally {
+                dialog.dismiss()
+            }
+        })
+        thread.start()
+
+
+        button.setOnClickListener{
+            toast(instagramUser.isLoggedIn.toString())
+        }
 
     }
 
