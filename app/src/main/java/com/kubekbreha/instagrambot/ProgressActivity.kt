ff82e1479225025d5317c04df3dc9962cbeff5dc
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-02 23:07:22 +0200

    get data from bundle

--- app/src/main/java/com/kubekbreha/instagrambot/ProgressActivity.kt
+++ app/src/main/java/com/kubekbreha/instagrambot/ProgressActivity.kt
@@ -5,8 +5,20 @@ import android.os.Bundle
 
 class ProgressActivity : AppCompatActivity() {
 
+    private var selectedListId : Int = -1
+    private var action: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_progress)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            selectedListId = bundle.get("listSelected") as Int
+            action = bundle.get("action") as String
+        }
+
+
+
     }
 }
