3a17bdccfc1ce630b0aebed013966656c90399c6
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-02 23:16:44 +0200

    add instagram functions to progress activity

--- app/src/main/java/com/kubekbreha/instagrambot/ProgressActivity.kt
+++ app/src/main/java/com/kubekbreha/instagrambot/ProgressActivity.kt
@@ -2,8 +2,16 @@ package com.kubekbreha.instagrambot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dev.niekirk.com.instagram4android.requests.*
+import dev.niekirk.com.instagram4android.requests.payload.InstagramFeedResult
+import dev.niekirk.com.instagram4android.requests.payload.InstagramSearchUsernameResult
+import dev.niekirk.com.instagram4android.requests.payload.InstagramUserSummary
 
 class ProgressActivity : AppCompatActivity() {
+
+    private val TAG = "BotNavDrawFrag"
+    lateinit var tagFeedClass: InstagramFeedResult
 
     private var selectedListId : Int = -1
     private var action: String = ""
@@ -17,8 +25,110 @@ class ProgressActivity : AppCompatActivity() {
             selectedListId = bundle.get("listSelected") as Int
             action = bundle.get("action") as String
         }
-
-
-
     }
+
+
+
+
+    private fun followListOfUsers(selectedList: Int){
+        val users = UsersInList(selectedList, this)
+        val namesOfUsers = users.getUsers()
+        for (user in namesOfUsers){
+            follow(user)
+            //[JB 1.7.2018]TODO add break for some time here
+        }
+    }
+
+
+
+    private fun unFollowListOfUsers(selectedList: Int){
+        val users = UsersInList(selectedList, this)
+        val namesOfUsers = users.getUsers()
+        for (user in namesOfUsers){
+            unFollow(user)
+            //[JB 1.7.2018]TODO add break for some time here
+        }
+    }
+
+
+    private fun follow(user: String) {
+        val thread = Thread(Runnable {
+            try {
+                val result = User.getUser().sendRequest(InstagramSearchUsernameRequest(user))
+                val user = result.user
+                User.getUser().sendRequest(InstagramFollowRequest(user.getPk()))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        })
+        thread.start()
+    }
+
+
+    private fun unFollow(user: String) {
+        val thread = Thread(Runnable {
+            try {
+                val result = User.getUser().sendRequest(InstagramSearchUsernameRequest(user))
+                val user = result.user
+                User.getUser().sendRequest(InstagramUnfollowRequest(user.getPk()))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        })
+        thread.start()
+    }
+
+
+
+
+    private fun like(pk: Long) {
+        val thread = Thread(Runnable {
+            try {
+                User.getUser().sendRequest(InstagramLikeRequest(pk))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        })
+        thread.start()
+    }
+
+
+    private fun getPostIDs(userName: String){
+        var tagFeed: InstagramFeedResult?
+        val thread = Thread(Runnable {
+            try {
+                val result = User.getUser().sendRequest(InstagramSearchUsernameRequest(userName))
+                tagFeed = User.getUser().sendRequest(InstagramUserFeedRequest(result.user.pk, 5.toString(), 50))
+                for (feedResult in tagFeed!!.items) {
+                    Log.e("POSTDEBUG", "Post ID: " + feedResult.getPk())
+                }
+                tagFeedClass = tagFeed!!
+                Log.e("POSTDEBUG", "Finished")
+
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        })
+        thread.start()
+    }
+
+
+    private fun getUserFollowers(userResult : InstagramSearchUsernameResult): MutableList<InstagramUserSummary>? {
+        val githubFollowers = User.getUser().sendRequest(InstagramGetUserFollowersRequest(userResult.user.getPk()))
+        val users = githubFollowers.getUsers()
+        for (user in users) {
+            println("User " + user.getUsername() + " follows " + userResult.user.username)
+        }
+        return users
+    }
+
+
+    private fun searchUser(userName: String): InstagramSearchUsernameResult {
+        val userResult = User.getUser().sendRequest(InstagramSearchUsernameRequest(userName))
+        Log.i(TAG,"ID for @"+ userResult.user.username +" " + userResult.user.getPk())
+        Log.i(TAG, "Number of followers: " + userResult.user.getFollower_count())
+        return userResult
+    }
+
 }
--- app/src/main/java/com/kubekbreha/instagrambot/fragments/BottomNavigationDrawerFragmentActions.kt
+++ app/src/main/java/com/kubekbreha/instagrambot/fragments/BottomNavigationDrawerFragmentActions.kt
@@ -22,8 +22,6 @@ import dev.niekirk.com.instagram4android.requests.InstagramUserFeedRequest
 
 class BottomNavigationDrawerFragmentActions: BottomSheetDialogFragment() {
 
-    private val TAG = "BotNavDrawFrag"
-    lateinit var tagFeedClass: InstagramFeedResult
     private var selectedList = -1
 
 
@@ -76,108 +74,6 @@ class BottomNavigationDrawerFragmentActions: BottomSheetDialogFragment() {
             // For example, swap UI fragments here
             true
         }
-    }
-
-
-    private fun followListOfUsers(selectedList: Int){
-        val users = UsersInList(selectedList, context!!)
-        val namesOfUsers = users.getUsers()
-        for (user in namesOfUsers){
-            follow(user)
-            //[JB 1.7.2018]TODO add break for some time here
-        }
-    }
-
-
-
-    private fun unFollowListOfUsers(selectedList: Int){
-        val users = UsersInList(selectedList, context!!)
-        val namesOfUsers = users.getUsers()
-        for (user in namesOfUsers){
-            unFollow(user)
-            //[JB 1.7.2018]TODO add break for some time here
-        }
-    }
-
-
-    private fun follow(user: String) {
-        val thread = Thread(Runnable {
-            try {
-                val result = User.getUser().sendRequest(InstagramSearchUsernameRequest(user))
-                val user = result.user
-                User.getUser().sendRequest(InstagramFollowRequest(user.getPk()))
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        })
-        thread.start()
-    }
-
-
-    private fun unFollow(user: String) {
-        val thread = Thread(Runnable {
-            try {
-                val result = User.getUser().sendRequest(InstagramSearchUsernameRequest(user))
-                val user = result.user
-                User.getUser().sendRequest(InstagramUnfollowRequest(user.getPk()))
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        })
-        thread.start()
-    }
-
-
-
-
-    private fun like(pk: Long) {
-        val thread = Thread(Runnable {
-            try {
-                User.getUser().sendRequest(InstagramLikeRequest(pk))
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        })
-        thread.start()
-    }
-
-
-    private fun getPostIDs(userName: String){
-        var tagFeed: InstagramFeedResult?
-        val thread = Thread(Runnable {
-            try {
-                val result = User.getUser().sendRequest(InstagramSearchUsernameRequest(userName))
-                tagFeed = User.getUser().sendRequest(InstagramUserFeedRequest(result.user.pk, 5.toString(), 50))
-                for (feedResult in tagFeed!!.items) {
-                    Log.e("POSTDEBUG", "Post ID: " + feedResult.getPk())
-                }
-                tagFeedClass = tagFeed!!
-                Log.e("POSTDEBUG", "Finished")
-
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        })
-        thread.start()
-    }
-
-
-    private fun getUserFollowers(userResult : InstagramSearchUsernameResult): MutableList<InstagramUserSummary>? {
-        val githubFollowers = User.getUser().sendRequest(InstagramGetUserFollowersRequest(userResult.user.getPk()))
-        val users = githubFollowers.getUsers()
-        for (user in users) {
-            println("User " + user.getUsername() + " follows " + userResult.user.username)
-        }
-        return users
-    }
-
-
-    private fun searchUser(userName: String): InstagramSearchUsernameResult {
-        val userResult = User.getUser().sendRequest(InstagramSearchUsernameRequest(userName))
-        Log.i(TAG,"ID for @"+ userResult.user.username +" " + userResult.user.getPk())
-        Log.i(TAG, "Number of followers: " + userResult.user.getFollower_count())
-        return userResult
     }
 
 }
