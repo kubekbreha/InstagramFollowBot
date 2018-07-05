affbf71454502712016be74d0b8279994fd09556
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-05 12:46:39 +0200

    can mark item in list with correct color

--- app/src/main/java/com/kubekbreha/instagrambot/adapters/ListsAdapter.kt
+++ app/src/main/java/com/kubekbreha/instagrambot/adapters/ListsAdapter.kt
@@ -1,20 +1,27 @@
 package com.kubekbreha.instagrambot.adapters
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.kubekbreha.instagrambot.AddListActivity
 import com.kubekbreha.instagrambot.R
+import kotlinx.android.synthetic.main.list_item.view.*
 
 
-class ListsAdapter(private val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+class ListsAdapter(private val items: ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
     private var doubleClicked = 0
     var markedItem = -100
+    private var itemsInAdapter: MutableList<CardView> = mutableListOf()
+
 
     // Gets the number of animals in the list
     override fun getItemCount(): Int {
@@ -27,10 +34,12 @@ class ListsAdapter(private val items : ArrayList<String>, val context: Context) 
     }
 
     // Binds each animal in the ArrayList to a view
+    @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-
         holder.listType.text = "#" + items[position]
+
+
         holder.itemView.setOnClickListener {
 
             //save to application class picked list
@@ -39,9 +48,11 @@ class ListsAdapter(private val items : ArrayList<String>, val context: Context) 
             editor.apply()
 
             doubleClicked++
-            if(position == markedItem) {
+            if (position == markedItem) {
                 if (doubleClicked == 2) {
                     resetDoubleClicked()
+                    var card = holder.itemView.list_item_cardView
+                    card.setCardBackgroundColor(context.resources.getColor(R.color.white))
 
                     val intent = Intent(context, AddListActivity::class.java)
                     val b = Bundle()
@@ -50,18 +61,30 @@ class ListsAdapter(private val items : ArrayList<String>, val context: Context) 
                     context.startActivity(intent)
                     (context as Activity).overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
                     (context).finish()
+
                 }
-            }else{
+            } else {
+                var card = holder.itemView.list_item_cardView
+                card.setCardBackgroundColor(context.resources.getColor(R.color.colorAccent))
+
+                itemsInAdapter.add(holder.itemView.list_item_cardView)
                 markedItem = position
-                if(doubleClicked == 2) doubleClicked = 0
+                if (doubleClicked == 2) doubleClicked = 0
+
+                if (itemsInAdapter.size >= 2) {
+                    Log.i("ADAPTERTAG", "IM IN")
+                    var card = itemsInAdapter[itemsInAdapter.size - 2]
+                    card.setCardBackgroundColor(context.resources.getColor(R.color.white))
+                }
             }
         }
     }
 
 
-    public fun resetDoubleClicked(){
+    fun resetDoubleClicked() {
         doubleClicked = 0
         markedItem = -100
     }
+
 
 }
