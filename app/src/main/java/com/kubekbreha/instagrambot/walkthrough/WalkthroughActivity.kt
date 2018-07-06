859a4a08cf66bd0dcd63c74bc3f094b78820795d
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-06 18:40:11 +0200

    dots are dotting :D by layouts

--- app/src/main/java/com/kubekbreha/instagrambot/walkthrough/WalkthroughActivity.kt
+++ app/src/main/java/com/kubekbreha/instagrambot/walkthrough/WalkthroughActivity.kt
@@ -2,20 +2,17 @@ package com.kubekbreha.instagrambot.walkthrough
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.Html
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.kubekbreha.instagrambot.LoginActivity
 import com.kubekbreha.instagrambot.R
+import com.kubekbreha.instagrambot.indicatorDots.SpringDotsIndicator
+import com.kubekbreha.instagrambot.indicatorDots.WormDotsIndicator
 import kotlinx.android.synthetic.main.activity_walkthrough.*
 
 
 class WalkthroughActivity : AppCompatActivity() {
 
-    private lateinit var slideViewPager: ViewPager
-    private lateinit var dotsLayout: LinearLayout
 
 
     private lateinit var sliderAdapter: WalkthroughSlideAdapter
@@ -24,20 +21,11 @@ class WalkthroughActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walkthrough)
 
-
-        slideViewPager = findViewById(R.id.activity_walkthrough_viewPager)
-        dotsLayout = findViewById(R.id.activity_walkthrough_dotsLayout)
-        sliderAdapter = WalkthroughSlideAdapter(this)
-        slideViewPager.adapter = sliderAdapter
-
-        addDotsIndicator(0)
-//        slideViewPager.addOnPageChangeListener(object : OnPageChangeListener {
-//            override fun onPageScrollStateChanged(state: Int) {}
-//            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-//            override fun onPageSelected(position: Int) {
-//                addDotsIndicator(position)
-//            }
-//        })
+        val wormDotsIndicator = findViewById<WormDotsIndicator>(R.id.activity_walkthrough_wormDotsLayout)
+        val viewPager = findViewById<ViewPager>(R.id.activity_walkthrough_viewPager)
+        val adapter = WalkthroughSlideAdapter(this)
+        viewPager.adapter = adapter
+        wormDotsIndicator.setViewPager(viewPager)
 
 
         activity_walkthrough_loginButton.setOnClickListener{
@@ -48,33 +36,5 @@ class WalkthroughActivity : AppCompatActivity() {
         }
 
     }
-
-    private fun addDotsIndicator(position: Int) {
-
-
-        val dots = arrayOfNulls<TextView>(3)
-
-        if(dotsLayout.getChildAt(0)!=null)
-            dotsLayout.removeViewAt(0)
-
-
-        if(dotsLayout.getChildAt(1)!=null)
-            dotsLayout.removeViewAt(1)
-
-        if(dotsLayout.getChildAt(2)!=null)
-            dotsLayout.removeViewAt(2)
-
-
-        for (i in 0 until 3) {
-            dots[i] = TextView(this)
-            dots[i]!!.text = Html.fromHtml("&#8226;")
-            dots[i]!!.textSize = 35F
-
-            dotsLayout.addView(dots[i], i)
-        }
-
-        dots[position]!!.setTextColor(resources.getColor(R.color.colorAccent))
-    }
-
 
 }
