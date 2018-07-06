8da4df538a5cbe9b07343773e0201ae741ca675d
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-06 13:14:44 +0200

    add static dots

--- app/src/main/java/com/kubekbreha/instagrambot/walkthrough/WalkthroughActivity.kt
+++ app/src/main/java/com/kubekbreha/instagrambot/walkthrough/WalkthroughActivity.kt
@@ -1,15 +1,20 @@
 package com.kubekbreha.instagrambot.walkthrough
 
 import android.os.Bundle
+import android.text.Html
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.kubekbreha.instagrambot.R
+import com.kubekbreha.instagrambot.UsersList
 
 class WalkthroughActivity : AppCompatActivity() {
 
     private lateinit var slideViewPager: ViewPager
     private lateinit var dotsLayout: LinearLayout
+
+    private var dots: MutableList<TextView> = mutableListOf()
 
     private lateinit var sliderAdapter: WalkthroughSlideAdapter
 
@@ -23,6 +28,24 @@ class WalkthroughActivity : AppCompatActivity() {
         sliderAdapter = WalkthroughSlideAdapter(this)
 
         slideViewPager.adapter = sliderAdapter
+
+        addDotsIndicator()
     }
 
+
+    private fun addDotsIndicator(){
+
+        for (item in 0..3) {
+            dots.add(item, TextView(this))
+        }
+
+        for (item in 1..3) {
+            dots[item].text = Html.fromHtml("&#8226;")
+            dots[item].textSize = 35F
+            dots[item].setTextColor(resources.getColor(R.color.white))
+
+            dotsLayout.addView(dots[item])
+        }
+
+    }
 }
