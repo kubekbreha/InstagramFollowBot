bb0014c8344720011f35a66ae728ed12d004c729
Jakub Brehuv <kubek.breha@gmail.com>
2018-07-06 16:20:47 +0200

    add dots which are not working correctly

--- app/src/main/java/com/kubekbreha/instagrambot/walkthrough/WalkthroughActivity.kt
+++ app/src/main/java/com/kubekbreha/instagrambot/walkthrough/WalkthroughActivity.kt
@@ -2,19 +2,22 @@ package com.kubekbreha.instagrambot.walkthrough
 
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.kubekbreha.instagrambot.R
 import com.kubekbreha.instagrambot.UsersList
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import java.text.FieldPosition
+
 
 class WalkthroughActivity : AppCompatActivity() {
 
     private lateinit var slideViewPager: ViewPager
     private lateinit var dotsLayout: LinearLayout
 
-    private var dots: MutableList<TextView> = mutableListOf()
 
     private lateinit var sliderAdapter: WalkthroughSlideAdapter
 
@@ -22,30 +25,53 @@ class WalkthroughActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walkthrough)
 
+
         slideViewPager = findViewById(R.id.activity_walkthrough_viewPager)
         dotsLayout = findViewById(R.id.activity_walkthrough_dotsLayout)
-
         sliderAdapter = WalkthroughSlideAdapter(this)
-
         slideViewPager.adapter = sliderAdapter
 
-        addDotsIndicator()
+        addDotsIndicator(0)
+        slideViewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                addDotsIndicator(position)
+            }
+        })
+
+    }
+
+    private fun addDotsIndicator(position: Int) {
+
+        Log.e("WTF", position.toString())
+
+        val dots = arrayOfNulls<TextView>(3)
+
+        if(dotsLayout.getChildAt(0)!=null)
+            dotsLayout.removeViewAt(0)
+
+
+        if(dotsLayout.getChildAt(1)!=null)
+            dotsLayout.removeViewAt(1)
+
+        if(dotsLayout.getChildAt(2)!=null)
+            dotsLayout.removeViewAt(2)
+
+
+        for (i in 0 until 3) {
+            dots[i] = TextView(this)
+            dots[i]!!.text = Html.fromHtml("&#8226;")
+            dots[i]!!.textSize = 35F
+            dots[i]!!.setTextColor(resources.getColor(R.color.white))
+            Log.e("WTFWTFWTFWTFWTF", i.toString())
+
+            dotsLayout.addView(dots[i], i)
+        }
+
+        dots[position]!!.setTextColor(resources.getColor(R.color.colorAccent))
     }
 
 
-    private fun addDotsIndicator(){
-
-        for (item in 0..3) {
-            dots.add(item, TextView(this))
-        }
-
-        for (item in 1..3) {
-            dots[item].text = Html.fromHtml("&#8226;")
-            dots[item].textSize = 35F
-            dots[item].setTextColor(resources.getColor(R.color.white))
-
-            dotsLayout.addView(dots[item])
-        }
-
-    }
 }
